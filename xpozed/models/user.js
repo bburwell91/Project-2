40827a8d9e5abeb64a625f61a8d9e860bd485809
Colin Lyons <Colinlyons53@gmail.com>
2019-07-25 12:29:59 -0700

    added crediility to the user model that takes in arrays.

--- xpozed/models/user.js
+++ xpozed/models/user.js
@@ -4,6 +4,7 @@ module.exports = function(sequelize, DataTypes) {
       password: DataTypes.STRING,
       email: DataTypes.STRING,
       created_at: DataTypes.DATE,
+      credibility: DataTypes.ARRAY,
       description: DataTypes.TEXT
 
     });
