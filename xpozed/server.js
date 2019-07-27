63fcbcb6a6e012b4a4ba906ad52877c8be73546c
Brooks Burwell <brooksdburwell@gmail.com>
2019-07-27 13:21:09 -0700

    pushing

--- xpozed/server.js
+++ xpozed/server.js
@@ -33,13 +33,13 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-io.on('connection', function (socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('a user disconnected')
+io.on("connection", function (socket){
+  console.log("a user connected");
+  socket.on("disconnect", function(){
+    console.log("a user disconnected")
   })
-  socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
+  socket.on("chat message", function(msg) {
+    console.log("message: " + msg);
   })
 });
 
